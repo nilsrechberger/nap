4f710ec6208892a4a471dab9824c4e399cd3f6ae
Nils Rechberger <nilsrechberger@outlook.com>
2026-04-01 11:00:46 +0200

    feat: extended country list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from email.contentmanager import raw_data_manager
+# Data handling
 import pandas as pd
 
+# Project packages
 from src.mvp.crawler import fetch_data
 from src.mvp.processor import *
 
@@ -8,18 +9,20 @@ if __name__ == '__main__':
 
     results = pd.DataFrame()
 
-    countries = [
-        'ch',
-        'de',
-        'at'
+    eu_countries = [
+        "at", "be", "bg", "cy", "cz", "de", "dk", "ee", "es", "fi", 
+        "fr", "gr", "hr", "hu", "ie", "it", "lt", "lu", "lv", "mt", 
+        "nl", "pl", "pt", "ro", "se", "si", "sk"
     ]
     
-    for country in countries:
+    for country in eu_countries:
         raw_data = fetch_data(country)
         table = as_table(raw_data)
         results = pd.concat([results, table], ignore_index=True)
+    
+    results.to_csv('data/raw/raw_data.csv')
 
     clean_results = remove_paid_content(results)
     print(clean_results)
 
-    clean_results.to_csv('data/mvp/mvp.csv')
+    clean_results.to_csv('data/processed/precessed_data.csv')
