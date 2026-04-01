7f16b90d8f266570fd8b657d9d411838cf05f825
Nils Rechberger <nilsrechberger@outlook.com>
2026-04-01 11:15:26 +0200

    feat: add timestamp to fetched data

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+
+# Build-in
+import os
+from datetime import date, datetime
+
 # Data handling
 import pandas as pd
 
@@ -5,26 +11,32 @@ import pandas as pd
 from src.crawler import fetch_data
 from src.processor import *
 
-def main():
+TIMESTAMP = datetime.now().date()
+
+def main() -> None:
     results = pd.DataFrame()
 
     eu_countries = [
+        "at"
+    ]
+
+    """ eu_countries = [
         "at", "be", "bg", "cy", "cz", "de", "dk", "ee", "es", "fi", 
         "fr", "gr", "hr", "hu", "ie", "it", "lt", "lu", "lv", "mt", 
         "nl", "pl", "pt", "ro", "se", "si", "sk"
-    ]
+    ] """
     
     for country in eu_countries:
         raw_data = fetch_data(country)
         table = as_table(raw_data)
         results = pd.concat([results, table], ignore_index=True)
     
-    results.to_csv('data/raw/raw_data.csv')
+    results.to_csv(f'data/raw/raw_data_{TIMESTAMP}.csv')
 
     clean_results = remove_paid_content(results)
     print(clean_results)
 
-    clean_results.to_csv('data/processed/precessed_data.csv')
+    clean_results.to_csv(f'data/processed/precessed_data_{TIMESTAMP}.csv')
 
 if __name__ == '__main__':
     main()
